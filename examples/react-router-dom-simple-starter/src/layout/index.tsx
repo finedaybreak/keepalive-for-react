8f9045d720ed639af032e47fecc7967bc094d4e4
wongyichen <wongyichen@163.com>
2026-04-20 21:54:56 +0800

    chore(example): bump react to 19.2.5 and enable Activity in layout

--- examples/react-router-dom-simple-starter/src/layout/index.tsx
+++ examples/react-router-dom-simple-starter/src/layout/index.tsx
@@ -37,7 +37,7 @@ function Layout() {
                         transition={true}
                         exclude={["/nocache-counter"]}
                         aliveRef={aliveRef}
-                        enableActivity={false}
+                        enableActivity={true}
                         // 虽然 Activity 可以提高性能，但是这里为了有淡出效果，禁用了19.2 的 Activity 组件
                     />
                 </CustomSuspense>
